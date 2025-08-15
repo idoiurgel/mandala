99524a22c08d92e7a5d6e0e364d000b0938ee12b
Mia <jugj@users.noreply.github.com>
2025-08-15 21:01:37 +0200

    Änderungen von Mia am 2025-08-15 21:01:37

---
+++ Assets/spielerbewegund.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spielerbewegund : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("up")){
+      transform.Translate(Vector2.up * Time.deltaTime);  
+      }
+      if(Input.GetKey("down")){transform.Translate(Vector2.down * Time.deltaTime);}
+      if(Input.GetKey("right")){transform.Translate(Vector2.right * Time.deltaTime);}
+      if(Input.GetKey("left")){transform.Translate(Vector2.left * Time.deltaTime);}
+    }
+}
