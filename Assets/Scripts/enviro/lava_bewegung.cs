bb15aa17389115023552d40a0d8a67122d1aa586
Mariia <jugj@users.noreply.github.com>
2025-08-16 19:51:19 +0200

    Änderungen von Mariia am 2025-08-16 19:51:19

--- Assets/Scripts/enviro/lava_bewegung.cs
+++ Assets/Scripts/enviro/lava_bewegung.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class lava_bewegung : MonoBehaviour
 {
     public Rigidbody2D l_rigidbody;
-    public GameObject player;
+    public Spieler player;
+    private Vector2 falling;
 
-    //public float lava_geschwindigkeit = 30f;
+    public void OnTriggerEnter2D(Collider2D other){
+        //deplete_leben();
+    }
+
     void Start()
     {
         
@@ -16,10 +20,17 @@ public class lava_bewegung : MonoBehaviour
 
     void Update()
     {
-        // if player is below lava, fall
-        if (player.transform.position.x >= this.transform.position.x){
-            
-            l_rigidbody.AddForce(Vector2.down, ForceMode2D.Impulse);
+        if(l_rigidbody != null){
+            if (l_rigidbody.transform.position.y < -19){
+                Destroy(l_rigidbody);
+            }
+            if (player.transform.position.x >= this.transform.position.x){ // if player is below lava, fall
+                falling = falling + Vector2.down;
+            }
+
+            l_rigidbody.AddForce(falling, ForceMode2D.Impulse);
         }
     }
+
+
 }
